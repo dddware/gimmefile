3c0a9c76c687864d1e374efadbcb0ba361a3e03e
neemzy <tom.panier@free.fr>
2013-12-28 23:14:37 +0100

    refactoring

--- app.js
+++ app.js
@@ -5,7 +5,6 @@
 
 var express = require('express')
   , routes = require('./routes')
-//  , user = require('./routes/user')
   , http = require('http')
   , path = require('path')
   , livereload = require('express-livereload');
@@ -31,7 +30,9 @@ app.configure('development', function(){
 });
 
 app.get('/', routes.index);
-app.post('/upload', routes.post);
+app.post('/create', routes.create);
+app.get('/bucket', routes.bucket);
+app.post('/upload', routes.upload);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
