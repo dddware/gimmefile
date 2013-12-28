1be09a2254bc76fd8e4f9ff18cb703abaf880216
neemzy <tom.panier@free.fr>
2013-12-28 22:34:41 +0100

    a l'arrache

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,12 @@
  * GET home page.
  */
 
-exports.index = function(req, res){
-  res.render('index', { title: 'Express' });
+exports.index = function(req, res) {
+    res.render('index');
+};
+
+
+
+exports.post = function(req, res) {
+    console.log(req.files);
 };
