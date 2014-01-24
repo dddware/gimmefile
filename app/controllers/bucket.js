71bd0756e3da6ee046af6b509633cbfad5aa2bfb
neemzy <tom.panier@free.fr>
2014-01-24 21:15:59 +0100

    added links on bucket page

--- app/controllers/bucket.js
+++ app/controllers/bucket.js
@@ -7,11 +7,11 @@ module.exports =
   // Display bucket page
   
   get: function(req, res, next) {
-    Bucket.findOne({ secret: req.params.secret }, 'name description', function (err, bucket) {
+    Bucket.findOne({ secret: req.params.secret }, 'name description secret', function (err, bucket) {
       if (! bucket) {
         next(new Error(404));
       } else {
-        res.render('bucket', { bucket: bucket });
+        res.render('bucket', { bucket: bucket, url: req.protocol + "://" + req.get('host'), uri: req.url });
       }
     });
   }
